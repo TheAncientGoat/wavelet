fc2dfddc6b2738938d605bb0757c6a8c197b83d5
Hasyimi Bahrudin <hasyimi@perlin.net>
2019-08-08 02:28:02 +0800

    ledger: update tests

--- ledger_test.go
+++ ledger_test.go
@@ -27,8 +27,8 @@ func TestLedger_BroadcastNop(t *testing.T) {
 	alice := testnet.AddNode(t, 1000000)
 	bob := testnet.AddNode(t, 0)
 
-	// Wait for alice to receive her PERL from the faucet
-	for <-alice.WaitForConsensus() {
+	// Wait for balance to update
+	for range alice.WaitForConsensus() {
 		if alice.Balance() > 0 {
 			break
 		}
@@ -125,155 +125,136 @@ func TestLedger_AddTransaction(t *testing.T) {
 	}
 }
 
-// func TestLedger_Pay(t *testing.T) {
-// 	testnet := NewTestNetwork(t)
-// 	defer testnet.Cleanup()
-//
-// 	alice := testnet.AddNode(t, 1000000)
-// 	bob := testnet.AddNode(t, 100)
-//
-// 	others := []*TestLedger{}
-// 	for i := 0; i < 5; i++ {
-// 		others = append(others, testnet.AddNode(t, 0))
-// 	}
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	assert.NoError(t, txError(alice.Pay(bob, 1237)))
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	// Bob should receive the tx amount
-// 	assert.EqualValues(t, 1337, bob.Balance())
-//
-// 	// Alice balance should be balance-txAmount-gas
-// 	aliceBalance := alice.Balance()
-// 	assert.True(t, aliceBalance < 1000000-1237)
-//
-// 	// Everyone else should see the updated balance of Alice and Bob
-// 	for _, node := range others {
-// 		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
-// 		assert.EqualValues(t, 1337, node.BalanceOfAccount(bob))
-// 	}
-// }
-//
-// func TestLedger_PayInsufficientBalance(t *testing.T) {
-// 	testnet := NewTestNetwork(t)
-// 	defer testnet.Cleanup()
-//
-// 	alice := testnet.AddNode(t, 1000000)
-// 	bob := testnet.AddNode(t, 100)
-//
-// 	others := []*TestLedger{}
-// 	for i := 0; i < 5; i++ {
-// 		others = append(others, testnet.AddNode(t, 0))
-// 	}
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	// Alice attempt to pay Bob more than what
-// 	// she has in her wallet
-// 	assert.NoError(t, txError(alice.Pay(bob, 1000001)))
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	// Bob should not receive the tx amount
-// 	assert.EqualValues(t, 100, bob.Balance())
-//
-// 	// Alice should have paid for gas even though the tx failed
-// 	aliceBalance := alice.Balance()
-// 	assert.True(t, aliceBalance > 0)
-// 	assert.True(t, aliceBalance < 1000000)
-//
-// 	// Everyone else should see the updated balance of Alice and Bob
-// 	for _, node := range others {
-// 		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
-// 		assert.EqualValues(t, 100, node.BalanceOfAccount(bob))
-// 	}
-// }
-//
-// func TestLedger_Gossip(t *testing.T) {
-// 	testnet := NewTestNetwork(t)
-// 	defer testnet.Cleanup()
-//
-// 	alice := testnet.AddNode(t, 1000000)
-// 	bob := testnet.AddNode(t, 100)
-//
-// 	for i := 0; i < 5; i++ {
-// 		testnet.AddNode(t, 0)
-// 	}
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	assert.NoError(t, txError(alice.Pay(bob, 1237)))
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	// When a new node joins the network, it will eventually receive
-// 	// all transactions in the network.
-// 	charlie := testnet.AddNode(t, 0)
-// 	timeout := time.NewTimer(15 * time.Second)
-// 	ticker := time.NewTicker(200 * time.Millisecond)
-//
-// 	for {
-// 		select {
-// 		case <-timeout.C:
-// 			t.Fatal("test timed out")
-//
-// 		case <-ticker.C:
-// 			if charlie.BalanceOfAccount(alice) == alice.Balance() &&
-// 				charlie.BalanceOfAccount(bob) == bob.Balance() {
-// 				return
-// 			}
-// 		}
-// 	}
-// }
-//
-// func TestLedger_Stake(t *testing.T) {
-// 	testnet := NewTestNetwork(t)
-// 	defer testnet.Cleanup()
-//
-// 	alice := testnet.AddNode(t, 1000000)
-//
-// 	others := []*TestLedger{}
-// 	for i := 0; i < 5; i++ {
-// 		others = append(others, testnet.AddNode(t, 0))
-// 	}
-//
-// 	testnet.WaitForConsensus(t)
-//
-// 	assert.NoError(t, txError(alice.PlaceStake(9001)))
-// 	testnet.WaitForConsensus(t)
-//
-// 	assert.EqualValues(t, 9001, alice.Stake())
-//
-// 	// Alice balance should be balance-stakeAmount-gas
-// 	aliceBalance := alice.Balance()
-// 	assert.True(t, aliceBalance < 1000000-9001)
-//
-// 	// Everyone else should see the updated balance of Alice
-// 	for _, node := range others {
-// 		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
-// 		assert.EqualValues(t, alice.Stake(), node.StakeOfAccount(alice))
-// 	}
-//
-// 	assert.NoError(t, txError(alice.WithdrawStake(5000)))
-// 	testnet.WaitForConsensus(t)
-//
-// 	assert.EqualValues(t, 4001, alice.Stake())
-//
-// 	// Withdrawn stake should be added to balance
-// 	oldBalance := aliceBalance
-// 	aliceBalance = alice.Balance()
-// 	assert.True(t, aliceBalance > oldBalance)
-//
-// 	// Everyone else should see the updated balance of Alice
-// 	for _, node := range others {
-// 		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
-// 		assert.EqualValues(t, alice.Stake(), node.StakeOfAccount(alice))
-// 	}
-// }
-//
+func TestLedger_Pay(t *testing.T) {
+	testnet := NewTestNetwork(t)
+	defer testnet.Cleanup()
+
+	alice := testnet.AddNode(t, 1000000)
+	bob := testnet.AddNode(t, 100)
+
+	for i := 0; i < 5; i++ {
+		testnet.AddNode(t, 0)
+	}
+
+	assert.NoError(t, txError(alice.Pay(bob, 1237)))
+
+	testnet.WaitForConsensus(t)
+
+	// Bob should receive the tx amount
+	assert.EqualValues(t, 1337, bob.Balance())
+
+	// Alice balance should be balance-txAmount-gas
+	aliceBalance := alice.Balance()
+	assert.True(t, aliceBalance < 1000000-1237)
+
+	// Everyone else should see the updated balance of Alice and Bob
+	for _, node := range testnet.Nodes() {
+		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
+		assert.EqualValues(t, 1337, node.BalanceOfAccount(bob))
+	}
+}
+
+func TestLedger_PayInsufficientBalance(t *testing.T) {
+	testnet := NewTestNetwork(t)
+	defer testnet.Cleanup()
+
+	alice := testnet.AddNode(t, 1000000)
+	bob := testnet.AddNode(t, 100)
+
+	for i := 0; i < 5; i++ {
+		testnet.AddNode(t, 0)
+	}
+
+	// Alice attempt to pay Bob more than what
+	// she has in her wallet
+	assert.NoError(t, txError(alice.Pay(bob, 1000001)))
+
+	testnet.WaitForConsensus(t)
+
+	// Bob should not receive the tx amount
+	assert.EqualValues(t, 100, bob.Balance())
+
+	// Alice should have paid for gas even though the tx failed
+	aliceBalance := alice.Balance()
+	assert.True(t, aliceBalance > 0)
+	assert.True(t, aliceBalance < 1000000)
+
+	// Everyone else should see the updated balance of Alice and Bob
+	for _, node := range testnet.Nodes() {
+		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
+		assert.EqualValues(t, 100, node.BalanceOfAccount(bob))
+	}
+}
+
+func TestLedger_Gossip(t *testing.T) {
+	testnet := NewTestNetwork(t)
+	defer testnet.Cleanup()
+
+	alice := testnet.AddNode(t, 1000000)
+	bob := testnet.AddNode(t, 100)
+
+	for i := 0; i < 5; i++ {
+		testnet.AddNode(t, 0)
+	}
+
+	assert.NoError(t, txError(alice.Pay(bob, 1237)))
+
+	testnet.WaitForConsensus(t)
+
+	// When a new node joins the network, it will eventually receive
+	// all transactions in the network.
+	charlie := testnet.AddNode(t, 0)
+
+	waitFor(t, "test timed out", func() bool {
+		return charlie.BalanceOfAccount(alice) == alice.Balance() &&
+			charlie.BalanceOfAccount(bob) == bob.Balance()
+	})
+}
+
+func TestLedger_Stake(t *testing.T) {
+	testnet := NewTestNetwork(t)
+	defer testnet.Cleanup()
+
+	alice := testnet.AddNode(t, 1000000)
+
+	for i := 0; i < 5; i++ {
+		testnet.AddNode(t, 0)
+	}
+
+	testnet.WaitForConsensus(t)
+
+	assert.NoError(t, txError(alice.PlaceStake(9001)))
+	testnet.WaitForConsensus(t)
+
+	assert.EqualValues(t, 9001, alice.Stake())
+
+	// Alice balance should be balance-stakeAmount-gas
+	aliceBalance := alice.Balance()
+	assert.True(t, aliceBalance < 1000000-9001)
+
+	// Everyone else should see the updated balance of Alice
+	for _, node := range testnet.Nodes() {
+		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
+		assert.EqualValues(t, alice.Stake(), node.StakeOfAccount(alice))
+	}
+
+	assert.NoError(t, txError(alice.WithdrawStake(5000)))
+	testnet.WaitForConsensus(t)
+
+	assert.EqualValues(t, 4001, alice.Stake())
+
+	// Withdrawn stake should be added to balance
+	oldBalance := aliceBalance
+	aliceBalance = alice.Balance()
+	assert.True(t, aliceBalance > oldBalance)
+
+	// Everyone else should see the updated balance of Alice
+	for _, node := range testnet.Nodes() {
+		assert.EqualValues(t, aliceBalance, node.BalanceOfAccount(alice))
+		assert.EqualValues(t, alice.Stake(), node.StakeOfAccount(alice))
+	}
+}
+
 // func TestLedger_Reward(t *testing.T) {
 // 	testnet := NewTestNetwork(t)
 // 	defer testnet.Cleanup()
@@ -281,31 +262,30 @@ func TestLedger_AddTransaction(t *testing.T) {
 // 	alice := testnet.AddNode(t, 1000000)
 // 	bob := testnet.AddNode(t, 1000000)
 // 	charlie := testnet.AddNode(t, 1000000)
-//
-// 	others := []*TestLedger{}
-// 	for i := 0; i < 10; i++ {
-// 		others = append(others, testnet.AddNode(t, 0))
-// 	}
+// 	dave := testnet.AddNode(t, 1000000)
+// 	erin := testnet.AddNode(t, 0)
 //
 // 	testnet.WaitForConsensus(t)
 //
 // 	assert.EqualValues(t, 0, alice.Reward())
 // 	assert.EqualValues(t, 0, bob.Reward())
+// 	assert.EqualValues(t, 0, charlie.Reward())
 //
 // 	assert.NoError(t, txError(alice.PlaceStake(500000)))
-// 	testnet.WaitForConsensus(t)
 // 	assert.NoError(t, txError(bob.PlaceStake(500000)))
-// 	testnet.WaitForConsensus(t)
 // 	assert.NoError(t, txError(charlie.PlaceStake(sys.MinimumStake-1)))
-// 	testnet.WaitForConsensus(t)
+// 	testnet.WaitForLatestConsensus(t)
 //
-// 	// Run through several rounds of consensus
-// 	from := []*TestLedger{alice, bob}
-// 	for i := 0; i < 10; i++ {
-// 		// Generate some transactions
-// 		assert.NoError(t, txError(from[i%2].Pay(others[i%10], uint64(rand.Intn(200)))))
-// 		testnet.WaitForConsensus(t)
+// 	// Generate some transactions
+// 	for i := 0; i < 5; i++ {
+// 		assert.NoError(t, txError(dave.Pay(erin, uint64(100)+uint64(rand.Intn(100)))))
+// 		<-dave.WaitForConsensus()
 // 	}
+//
+// 	testnet.WaitForRound(t, dave.RoundIndex())
+//
+// 	fmt.Println(alice.Reward())
+// 	fmt.Println(bob.Reward())
 //
 // 	// Alice and Bob should have received some rewards
 // 	assert.True(t, alice.Reward() > 0)
